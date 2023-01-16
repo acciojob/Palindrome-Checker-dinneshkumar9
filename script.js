d0e894f0d01c1b029376fbcf1beaba22277d460d
Dinesh kumar <dinneshkumar9@gmail.com>
2023-01-16 22:47:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 // complete the given function
 
 function palindrome(str){
-
+	//convert the string in lower cases
+	str = str.toLowerCase();
+	//reverse the string
+	let reversedStr = str.split('').reverse().join('');
+	//check if the str is palindrome or not
+	return str === reversedStr;
 }
 module.exports = palindrome
